fa3fbecfc40359696f533f30c827ed09f73ee058
Cosmel Villalobos <cosmelvg@gmail.com>
2021-01-27 00:37:35 -0500

    added the correct imported files

--- src/page_load.js
+++ src/page_load.js
@@ -1,5 +1,6 @@
 import _ from "lodash";
-
+import "../css/style.css";
+import Icon from "../images";
 const pageLoad = (() => {
   //container
   const contentContainer = document.querySelector("#content");
@@ -40,6 +41,38 @@ const pageLoad = (() => {
   </nav>`;
 
   contentContainer.appendChild(header);
+
+  const tabs = document.createElement("tabs");
+  tabs.innerHTML = `
+  <h2>More information</h2>
+  
+  <div class="tabs">
+    <ul>
+      <li class="trigger active" data-target="#about">About</li>
+      <li class="trigger" data-target="#delivery">Delivery info</li>
+      <li class="trigger" data-target="#returns">Returns info</li>
+    </ul>
+    <div id="about" class="content active">
+      <h3>about</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima suscipit assumenda mollitia nostrum atque facilis neque, non blanditiis deleniti, ab numquam similique eligendi soluta necessitatibus, ex quibusdam illum eaque voluptate.</p>
+    </div>
+    <div id="delivery" class="content">
+      <h3>delivery</h3>
+      <p>Non blanditiis deleniti, ab numquam similique eligendi soluta necessitatibus, ex quibusdam illum eaque voluptate.Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima suscipit assumenda mollitia nostrum atque facilis neque.</p>
+    </div>
+    <div id="returns" class="content">
+      <h3>returns</h3>
+      <p>Adipisicing elit. Minima suscipit assumenda mollitia nostrum atque facilis neque, non blanditiis deleniti, lorem ipsum dolor sit amet consectetur ab numquam similique eligendi soluta necessitatibus, ex quibusdam illum eaque voluptate.</p>
+    </div>
+  </div>
+</div> `;
+  contentContainer.appendChild(tabs);
+
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
 })();
 
 export { pageLoad };
