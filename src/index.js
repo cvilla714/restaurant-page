0ceef0c5c81723c40388ff18dcc20eaca07100af
Cosmel Villalobos <cosmelvg@gmail.com>
2021-01-28 16:45:43 -0500

    added the galery and nav files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-// import { pageLoad } from "./page_load";
+import { pageLoad } from "./page_load";
 import { tabsLoad, Tabs } from "./tabs";
 import { navCnt } from "./nav";
-
+import { galeryLoad } from "./galery";
+// import { mainnavCnt } from "./mainnav";
 // create tabs
 const tabs = new Tabs(document.querySelector(".tabs"));
 tabs.init();
